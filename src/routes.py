7cde8fa57d69f606d18091fa063ae8cf237d0601
rijan adhikari <rijanadhikari@gmail.com>
2022-05-11 11:25:47 +0900

    added seperate file for route registration

---
+++ src/routes.py
@@ -0,0 +1,18 @@
+from src.resources.auth import AuthView
+from src.resources.user import UsersView
+from src.resources.docs import DocsView
+from src.resources.default_view import DefaultView
+from src.extensions import apispec
+
+
+def register(app):
+    UsersView.register(app, trailing_slash=False)
+    AuthView.register(app, trailing_slash=False)
+    DocsView.register(app, trailing_slash=False)
+    DefaultView.register(app, trailing_slash=False)
+
+    with app.test_request_context():
+        apispec.paths(AuthView, app)
+
+    jwt_scheme = {"type": "http", "scheme": "bearer", "bearerFormat": "JWT"}
+    apispec.components.security_scheme("jwt", jwt_scheme)
