3b0feffc40f2989f389e6af58213d39f1cf84624
rijan adhikari <rijanadhikari@gmail.com>
2022-05-11 11:11:26 +0900

    added login response schema

--- src/schemas/auth.py
+++ src/schemas/auth.py
@@ -1,11 +1,18 @@
 from marshmallow import fields
 
 from src.extensions import ma
+from src.schemas.user import UserSchema
 
 
 class LoginSchema(ma.Schema):
     username = fields.String(required=True)
     password = fields.String(required=True)
+
+
+class LoginResponseSchema(ma.Schema):
+    access_token = fields.String()
+    refresh_token = fields.String()
+    user = ma.Nested(UserSchema)
 
 
 class ResetPasswordSchema(ma.Schema):
