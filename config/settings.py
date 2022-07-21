164b2c5aaab711ccb04b7fc3a072051106201a37
rijan adhikari <rijanadhikari@gmail.com>
2022-07-21 15:43:25 +0900

    cleanup

--- config/settings.py
+++ config/settings.py
@@ -1,13 +1,6 @@
 import os
 from distutils.util import strtobool
 
-from dotenv import load_dotenv
-
-# Load env var from dotenv file
-APP_ROOT = os.path.join(os.path.dirname(__file__), '..')
-env_path = os.path.join(APP_ROOT, '.env')
-load_dotenv(env_path)
-
 DEBUG = True
 
 FLASK_APP_VERSION = os.getenv('FLASK_APP_VERSION')
